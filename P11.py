5e078248e768810ba9036ff254bdc17d24bbb288
Jay Oatts <jay.oatts@gmail.com>
2014-06-12 16:15:39 -0400

    adding P11 solution, updating README, adding createFile.py

--- P11.py
+++ P11.py
@@ -2,19 +2,19 @@
 print ("Project Euler - Problem 11\n\n")
 # ==============================================================================
 # Description:
-# In the 20x20 grid below, four numbers along a diagonal line have been marked 
+# In the 20 x 20 grid below, four numbers along a diagonal line have been marked 
 # in red.
-# 
-# 08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
-# 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
-# 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+#
+# 08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 
+# 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 
+# 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 
 # 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
 # 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
 # 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
 # 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
 # 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
 # 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
-# 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+# 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 
 # 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
 # 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
 # 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
@@ -25,11 +25,10 @@ print ("Project Euler - Problem 11\n\n")
 # 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
 # 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
 # 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
-# 
-# The product of these numbers is 26 x 63 x 78 x 14 = 1788696.
-# 
+#
+# The product of these numbers is 26 * 63 * 78 * 14 = 1788696.
 # What is the greatest product of four adjacent numbers in the same direction 
-# (up, down, left, right, or diagonally) in the 20x20 grid?
+# (up, down, left, right, or diagonally) in the 20 x 20 grid?
 # ==============================================================================
 import time
 import math
@@ -40,6 +39,7 @@ result = 0
 
 LENGTH = 20;
 HEIGHT = 20;
+ADJACENT = 4;
 
 grid = [
 [ 8, 2,22,97,38,15, 0,40, 0,75, 4, 5, 7,78,52,12,50,77,91, 8],
@@ -64,9 +64,50 @@ grid = [
 [ 1,70,54,71,83,51,54,69,16,92,33,48,61,43,52, 1,89,19,67,48]
 ]
 
+max_prod = 0
+product = 0
+
 for i in range (0,HEIGHT):
     for j in range (0,LENGTH):
-        print(grid[i][j])
+        # print("[" + str(i) + "," + str(j) + "]: ", end="")
+        if (i <= HEIGHT-ADJACENT):
+            # TODO optimize with filtering out multiply by zeros
+            #print(grid[i][j], grid[i+1][j], grid[i+2][j], grid[i+3][j], end="")
+            # DOWN
+            product = grid[i][j] * grid[i+1][j] * grid[i+2][j] * grid[i+3][j]
+            if (product > max_prod):
+                max_prod = product
+
+            if (j <= LENGTH-ADJACENT):
+                # print("R DR ", end="")
+                # RIGHT
+                product = grid[i][j] * grid[i][j+1] * grid[i][j+2] * grid[i][j+3]
+                if (product > max_prod):
+                    max_prod = product
+                # DOWN-RIGHT
+                product = grid[i][j] * grid[i+1][j+1] * grid[i+2][j+2] * grid[i+3][j+3]
+                if (product > max_prod):
+                    max_prod = product
+
+            if (j >= ADJACENT-1): 
+                # print("DL ", end="")
+                # DOWN-RIGHT
+                product = grid[i][j] * grid[i+1][j-1] * grid[i+2][j-2] * grid[i+3][j-3]
+                if (product > max_prod):
+                    max_prod = product
+
+
+        else:
+            if (j <= LENGTH-ADJACENT):
+                # print("R ", end="")
+                # RIGHT
+                product = grid[i][j] * grid[i][j+1] * grid[i][j+2] * grid[i][j+3]
+                if (product > max_prod):
+                    max_prod = product
+
+        # print("Max is: ", max_prod)
+
+result = max_prod
 
 # ***** END CODE FOR P11.py *****
 print ("Result is: ", result)
---
+++ createFile.py
@@ -0,0 +1,114 @@
+# createFile.py
+# Creates a header file for a new Project Euler problem. Input the problem 
+# number at the command line to create a new file:
+# $ python createFile.py <n>
+# If file exists, the program asks for confirmation to overwrite, otherwise
+# the file is automatically created. 
+
+import urllib.request
+import sys
+import os
+import re
+
+from html.parser import HTMLParser
+
+# HTML Parser to pull in the data in <p> tags
+class MyHTMLParser(HTMLParser):
+    def __init__(self, *args, **kwargs):
+        HTMLParser.__init__(self)
+        self.startPrint = False
+        self.textData = ''
+    def handle_starttag(self, tag, attrs):
+        if (tag == "p"):
+            self.startPrint = True           
+    def handle_data(self, data):
+        if (self.startPrint):
+            self.textData += data
+    def handle_endtag(self, tag):
+        if (tag == "p"):
+            self.startPrint = False
+            self.textData += '\n'
+
+# Pull in problem number from first argument of command line and append to file
+probnum = str(sys.argv[1])
+filename = "P" + probnum + ".py"
+
+# Check if file exists. If yes, asks for confirmation to overwrite or exits. If
+# no, the file is automatically created
+if (os.path.exists(filename)):
+    ret = input("File " + filename + " exists. Would you like to overwrite? [Y/n]: ")
+    if (ret == "Y"):
+        print ("Overwriting",filename)
+        f = open(filename, "w")
+    else:
+        print ("Exiting without overwriting",filename)
+        sys.exit()
+else:
+    print ("No file named", filename, "detected in directory, creating new file.")
+    f = open(filename, "w")
+
+#Print header to new file
+f.write('# ==============================================================================\n')
+f.write('print ("Project Euler - Problem ')
+f.write(probnum)
+f.write('\\n\\n")\n')
+f.write('# ==============================================================================\n')
+f.write('# Description:\n')
+
+# Create the URL address and open it with urllib.request. Save the source as a 
+# string to be parsed.
+addr = "http://projecteuler.net/problem=" + probnum
+url = urllib.request.urlopen(addr)
+source = str(url.readlines())
+
+# Create instance of HTML Parser and feed the source file to be parsed
+parser = MyHTMLParser()
+parser.feed(source)
+
+# Take raw string and split on newlines and spaces. () around delimiter to 
+# retain delimiter. Filter list for empty strings "None".
+wordList = re.split('( )|(\n)', parser.textData)
+wordList = filter(None, wordList)
+
+# Initialize character count to wrap lines at 80 characters
+charCount = 0
+
+# Loop through words for description section. If character count + length of 
+# next word exceeds 78 (each line starts with two characters "# ") then insert 
+# newline and reset count. If newline detected, write it and reset count. If 
+# count is zero, add the comment and space to beginning of line. Always write 
+# the word and add the length of it to the count.
+for i in wordList:   
+    if ((charCount + len(i)) > 78):
+        f.write('\n')
+        charCount = 0
+    if (i == '\n'):
+        charCount = 0
+        f.write(i)
+        continue 
+    if (charCount == 0):
+        f.write('# ')  
+    f.write(i)
+    charCount += len(i)
+
+# Close out the header and add the result and print section
+f.write('# ==============================================================================\n')
+f.write('import time\nimport math\n\n')
+f.write('start_time = time.time()\n')
+f.write('result = 0\n')
+f.write('# ***** BEGIN CODE FOR ' + filename + ' *****\n\n\n\n')
+f.write('# ***** END CODE FOR ' + filename + ' *****\n')
+f.write('print ("Result is: ", result)\n')
+f.write('print ("Run time: " + str((time.time() - start_time)) + " seconds")\n')
+f.write('# END ' + filename)
+
+# Close the URL and file
+url.close()
+f.close()
+
+# Indicate success
+print (filename + " template file created!")
+
+# Cleanup old files
+print ('Cleaning up backup files...')
+os.system("rm *.py~")
